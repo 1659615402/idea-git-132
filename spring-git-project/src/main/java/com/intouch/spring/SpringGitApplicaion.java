013671f3d8df6aa8a23153cfb6576aaed8564a06
lishaopeng <1659615402@qq.com>
2022-01-20 14:52:25 +0800

    idea创建spring-git-project并放入版本库中

---
+++ spring-git-project/src/main/java/com/intouch/spring/SpringGitApplicaion.java
@@ -0,0 +1,16 @@
+package com.intouch.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author:李少鹏
+ * @date:2022/1/20,14:46
+ * @version:1.0
+ */
+public class SpringGitApplicaion {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        System.out.println("helloGit");
+    }
+}
